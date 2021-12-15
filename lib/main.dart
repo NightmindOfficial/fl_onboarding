b14e0b3384db6c0bac0e285dc3371d8b8d9fd020
NightmindOfficial <mail@otismohr.de>
2021-12-15 21:56:05 +0100

    Basic Functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,215 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_svg/svg.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+          textTheme: GoogleFonts.ralewayTextTheme(
+        Theme.of(context).textTheme,
+      )),
+      home: Scaffold(
+        body: Container(
+          child: LoginPage(),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _LoginPageState createState() => _LoginPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _LoginPageState extends State<LoginPage> {
+  int _pageState = 0;
+  Color _backgroundColor = Colors.white;
+  Color _headingColor = Colors.grey;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  double _loginYOffset = 0;
+  double _loginXOffset = 0;
+  double _registerYOffset = 0;
+  double _loginWidth = 0;
+  double _loginOpacity = 1;
+  double windowWidth = 0;
+  double windowHeight = 0;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    windowHeight = MediaQuery.of(context).size.height;
+    windowWidth = MediaQuery.of(context).size.width;
+
+    switch (_pageState) {
+      case 0:
+        _backgroundColor = Colors.white;
+        _headingColor = Colors.grey;
+        _loginYOffset = windowHeight;
+        _registerYOffset = windowHeight;
+        _loginXOffset = 0;
+        _loginWidth = windowWidth;
+        _loginOpacity = 1;
+        break;
+      case 1:
+        _backgroundColor = const Color(0XFF79BC4E);
+        _headingColor = Colors.white;
+        _loginYOffset = 200;
+        _registerYOffset = windowHeight;
+        _loginXOffset = 0;
+        _loginWidth = windowWidth;
+        _loginOpacity = 1;
+        break;
+      case 2:
+        _backgroundColor = Color(0XFF3AAA35);
+        _headingColor = Colors.white;
+        _loginYOffset = 170;
+        _registerYOffset = 200;
+        _loginXOffset = 20;
+        _loginWidth = windowWidth - 40;
+        _loginOpacity = 0.80;
+    }
+
+    return SafeArea(
+      child: Stack(
+        children: [
+          AnimatedContainer(
+            duration: const Duration(milliseconds: 1000),
+            curve: Curves.fastLinearToSlowEaseIn,
+            color: _backgroundColor,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
+                  onTap: () => setState(() {
+                    _pageState = 0;
+                  }),
+                  child: Container(
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          top: 64.0, left: 36.0, right: 36.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Welcome to this Test App.',
+                            style:
+                                TextStyle(fontSize: 28, color: _headingColor),
+                            textAlign: TextAlign.center,
+                          ),
+                          Text(
+                            "Lorem ipsum is so out of date. Instead, I have written a fancy description text all by myself! Cool, right?",
+                            style:
+                                TextStyle(fontSize: 16, color: _headingColor),
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 36),
+                  child: SvgPicture.asset('assets/Test.svg'),
+                  height: 400,
+                ),
+                GestureDetector(
+                  onTap: () => setState(() {
+                    _pageState = 1;
+                    print(_pageState);
+                  }),
+                  child: Container(
+                    margin: EdgeInsets.all(24),
+                    padding: EdgeInsets.all(16),
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        color: Colors.greenAccent,
+                        borderRadius: BorderRadius.circular(32)),
+                    child: Text(
+                      'Get started',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.grey[700],
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+          GestureDetector(
+            onTap: () => setState(() {
+              _pageState = 2;
+            }),
+            child: AnimatedContainer(
+              width: _loginWidth,
+              curve: Curves.fastLinearToSlowEaseIn,
+              duration: Duration(milliseconds: 1100),
+              transform:
+                  Matrix4.translationValues(_loginXOffset, _loginYOffset, 1),
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(_loginOpacity),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(32),
+                  topRight: Radius.circular(32),
+                ),
+              ),
+              child: Column(
+                children: [
+                  PrimaryButton(),
+                ],
+              ),
             ),
-          ],
-        ),
+          ),
+          GestureDetector(
+            onTap: () => setState(() {
+              _pageState = 1;
+            }),
+            child: AnimatedContainer(
+              curve: Curves.fastLinearToSlowEaseIn,
+              duration: Duration(milliseconds: 1100),
+              transform: Matrix4.translationValues(0, _registerYOffset, 0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(32),
+                  topRight: Radius.circular(32),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class PrimaryButton extends StatefulWidget {
+  const PrimaryButton({Key? key}) : super(key: key);
+
+  @override
+  _PrimaryButtonState createState() => _PrimaryButtonState();
+}
+
+class _PrimaryButtonState extends State<PrimaryButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20),
+      child: Center(
+        child: Text('Get Started'),
+      ),
     );
   }
 }
